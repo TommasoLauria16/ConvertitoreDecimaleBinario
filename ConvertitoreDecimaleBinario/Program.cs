aa7b917ea4432ca548f0aa9bc716e569ea312e9d
tommaso lauria <tommaso.lauria9@gmail.com>
2020-10-30 10:04:15 +0100

    definitivo una solo funzione

--- ConvertitoreDecimaleBinario/Program.cs
+++ ConvertitoreDecimaleBinario/Program.cs
@@ -6,7 +6,7 @@ namespace ConvertitoreDecimaleBinario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter the base you want to convert to");
+            Console.WriteLine("enter the base you want to convert to:");
             int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"enter a decimal number you want to convert to base{b}");
             int decimale = Convert.ToInt32(Console.ReadLine());
