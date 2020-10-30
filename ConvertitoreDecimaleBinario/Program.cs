b5e86e4c30cea4c183803ccff9aa991b6c906ab0
tommaso lauria <tommaso.lauria9@gmail.com>
2020-10-30 10:02:24 +0100

    Add project files.

---
+++ ConvertitoreDecimaleBinario/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConvertitoreDecimaleBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the base you want to convert to");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"enter a decimal number you want to convert to base{b}");
+            int decimale = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(decimale,2));
+        }
+    }
+}
