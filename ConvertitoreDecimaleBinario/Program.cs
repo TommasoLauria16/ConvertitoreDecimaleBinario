91dbfc619866dc007eb3eec63c0a20132ce986a4
tommaso lauria <tommaso.lauria9@gmail.com>
2020-10-30 10:10:57 +0100

    definitivo

--- ConvertitoreDecimaleBinario/Program.cs
+++ ConvertitoreDecimaleBinario/Program.cs
@@ -8,9 +8,9 @@ namespace ConvertitoreDecimaleBinario
         {
             Console.WriteLine("enter the base you want to convert to:");
             int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"enter a decimal number you want to convert to base{b}");
+            Console.WriteLine($"enter a decimal number you want convert :");
             int decimale = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(decimale,2));
+            Console.WriteLine(Convert.ToString(decimale,b));
         }
     }
 }
