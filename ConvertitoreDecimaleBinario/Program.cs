18917244b5abe287a35202b6ae257b86b6668036
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-09 09:09:49 +0100

    Creazione di un ramo di sviluppo con inserimento della funzione PadLeft

--- ConvertitoreDecimaleBinario/Program.cs
+++ ConvertitoreDecimaleBinario/Program.cs
@@ -6,11 +6,12 @@ namespace ConvertitoreDecimaleBinario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter the base you want to convert to:");
-            int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"enter a decimal number you want convert :");
             int decimale = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(decimale,b));
+            Console.WriteLine("enter the base you want to convert to:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            string numero = Convert.ToString(decimale, b).PadLeft(8,'0');
+            Console.WriteLine(numero);
         }
     }
 }
